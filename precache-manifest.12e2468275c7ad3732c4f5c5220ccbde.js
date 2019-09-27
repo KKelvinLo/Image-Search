b1356f6d8c759f55ac2fe8c2a8abec09eddd4292
Kelvin Lo <kello@live.ca>
2019-09-27 04:37:35 -0700

    Updates

---
+++ precache-manifest.12e2468275c7ad3732c4f5c5220ccbde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab9276038f69a7d63d6084934061d2d",
+    "url": "/Image-Search/index.html"
+  },
+  {
+    "revision": "935734432f5451049225",
+    "url": "/Image-Search/static/css/main.f8921964.chunk.css"
+  },
+  {
+    "revision": "abf3b3417cd3b02a7278",
+    "url": "/Image-Search/static/js/2.018824d6.chunk.js"
+  },
+  {
+    "revision": "935734432f5451049225",
+    "url": "/Image-Search/static/js/main.05c9cdb3.chunk.js"
+  },
+  {
+    "revision": "29e34182b97280d5b7ed",
+    "url": "/Image-Search/static/js/runtime-main.de32557b.js"
+  }
+]);
