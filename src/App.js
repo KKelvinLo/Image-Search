704f0cb2a5f74fe6413da321f5ef9528d76c1ebf
Kelvin Lo <kello@live.ca>
2019-09-27 17:02:00 -0700

    update file to publish on github pages

--- src/App.js
+++ src/App.js
@@ -19,11 +19,9 @@ export default class App extends Component {
 
   //Could use .env file to keep api key private & seperate with back-end if necessary.
   async onSubmit(search) {
-    const Apikey =
-      "3b865726253202d2a6ab1984aeeab2c747e191ded0b5f29e64d0f9ee0fe414d6";
     await axios
       .get(
-        `https://api.unsplash.com/search/photos/?client_id=${Apikey}&per_page=100&orientation=landscape&query=${search}}`,
+        `https://api.unsplash.com/search/photos/?client_id=$e3b3207e3bfcb7d51c9ab7c8b5a7b1d2c03bd8b3787bd22d7e66820ce5f2145e&per_page=100&orientation=landscape&query=${search}}`,
         {
           params: { query: search }
         }
